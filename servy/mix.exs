5bd69301633bb97dc2ba179ee4aa2d2137f5a076
Simon Landry <simon.landry@rumandcode.io>
2025-01-13 13:28:42 -0500

    Upgrade Elixir version

--- servy/mix.exs
+++ servy/mix.exs
@@ -5,7 +5,7 @@ defmodule Servy.MixProject do
     [
       app: :servy,
       version: "0.1.0",
-      elixir: "~> 1.16.2",
+      elixir: "~> 1.18.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
